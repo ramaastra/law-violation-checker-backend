87950910a9b2c6d0f10951fe5961fda5fe817c5f
Rama Astra <ramaastra333@gmail.com>
2024-06-22 16:28:57 +0800

    feat: add train_knn() function to create KNN model

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from db import init_db
+from db import init_db, get_connection
+from model import train_knn
 
 if init_db():
     print("Database initialized.")
 else:
     print("Something was wrong in database initialization.")
+
+conn = get_connection()
+cursor = conn.cursor()
+cursor.execute("SELECT text, label FROM cases;")
+data = cursor.fetchall()
+
+cases = {"text": [], "label": []}
+for text, label in data:
+    cases["text"].append(text)
+    cases["label"].append(label)
+
+train_knn(cases)
+
+
+cursor.close()
+conn.close()
--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,11 @@
 import preprocess
+import joblib
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from imblearn.over_sampling import SMOTE
 
 
 def preprocess_data(text):
@@ -12,3 +19,44 @@ def preprocess_data(text):
     clean_text = preprocess.remove_extra_spaces(clean_text)
 
     return clean_text.strip()
+
+
+def train_knn(data):
+    texts = data.get("text")
+    labels = data.get("label")
+
+    tfidf_vectorizer = TfidfVectorizer()
+    tdm = tfidf_vectorizer.fit_transform(texts)
+
+    smote = SMOTE(random_state=42)
+    X, y = smote.fit_resample(tdm, labels)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, stratify=y)
+
+    print("\n> Creating KNN model.")
+
+    k_values = [k for k in range(1, 6)]
+    scores = []
+
+    for k in k_values:
+        print(f"  > Searching best value of k (current k={k})...")
+        knn = KNeighborsClassifier(n_neighbors=k, metric="euclidean")
+        score = cross_val_score(knn, X, y, cv=3)
+        scores.append(np.mean(score))
+
+    best_index = np.argmax(scores)
+    best_k = k_values[best_index]
+
+    knn = KNeighborsClassifier(n_neighbors=best_k, metric="euclidean")
+    knn.fit(X_train, y_train)
+
+    print(f"\n> KNN model created with k={best_k}.")
+
+    y_pred = knn.predict(X_test)
+    print("\n", classification_report(y_test, y_pred))
+
+    joblib.dump(knn, "./model/trained_models/knn.pkl")
+    joblib.dump(tfidf_vectorizer, "./model/trained_models/vectorizer.pkl")
+
+    print("\n> Model saved in model/trained_models/knn.pkl")
+    print("> Vectorizer saved in model/trained_models/vectorizer.pkl")
