bfdbcc0094f74ee114a0776dd9a8e251b0f039e4
Rama Astra <ramaastra333@gmail.com>
2024-06-22 17:45:33 +0800

    feat: init Flask app and add GET /init-db endpoint to initialize database

--- app.py
+++ app.py
@@ -1,42 +1,21 @@
-from db import init_db, get_connection
-from model import train_knn, predict_case
+from flask import Flask
+from db import init_db
 
-if init_db():
-    print("Database initialized.")
-else:
-    print("Something was wrong in database initialization.")
-
-conn = get_connection()
-cursor = conn.cursor()
-cursor.execute("SELECT text, label FROM cases;")
-data = cursor.fetchall()
-
-cases = {"text": [], "label": []}
-for text, label in data:
-    cases["text"].append(text)
-    cases["label"].append(label)
-
-train_knn(cases)
+app = Flask(__name__)
 
 
-cursor.close()
-conn.close()
+@app.route("/")
+def root():
+    return "Server connected"
 
 
-test_text = [
-    "Kamu orangnya baik banget!",
-    "Kamu orangnya BANGSAT banget!",
-    "mati aja lo sana",
-    "pemerintah ngentot",
-    "Babi den",
-    "Bupati orang bodoh",
-]
+@app.route("/init-db")
+def init():
+    if init_db():
+        return "Database initialized successfully"
+    else:
+        return "Database initialization error"
 
-print("======================================================\n")
-for text in test_text:
-    label, desc, proba = predict_case(text)
-    print(text)
-    print("> Label:", label)
-    print("> Description:", desc)
-    print("> Probability:", proba)
-    print("\n======================================================\n")
+
+if __name__ == "__main__":
+    app.run(debug=True)
