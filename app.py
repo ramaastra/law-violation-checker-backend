499c16df740f36e51bbaecda5b052cc6d34591b9
Rama Astra <ramaastra333@gmail.com>
2024-06-22 17:53:58 +0800

    feat: add GET /labels endpoint to fetch all labels

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from db import init_db
+from controller import label as label_controller
 
 app = Flask(__name__)
 
@@ -15,6 +16,12 @@ def init():
         return "Database initialized successfully"
     else:
         return "Database initialization error"
+
+
+@app.route("/labels")
+def get_all():
+    labels = label_controller.get_all()
+    return labels
 
 
 if __name__ == "__main__":
--- controller/__init__.py
+++ controller/__init__.py
@@ -1,7 +1,23 @@
 from db import get_connection
 
 
+def format_tuples(keys, list_of_tuples):
+    list_of_dict = [dict(zip(keys, values)) for values in list_of_tuples]
+    return list_of_dict
+
+
 class label:
+    def get_all():
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM labels")
+        data = cursor.fetchall()
+        cursor.close()
+        conn.close()
+
+        labels = format_tuples(("id", "title", "description"), data)
+        return labels
+
     def get_detail(id):
         conn = get_connection()
         cursor = conn.cursor()
